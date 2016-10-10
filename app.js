f40383e3d0a9ca133c3b200abb3442cbc1513116
Axel <lezinaxel@gmail.com>
2016-10-10 13:30:26 +0200

    Début de la To Do List

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+
+	// $( ".container" ).delegate( "button", "click", function() {
+	// 	$(this).after('<input type="text" id="input" placeholder="Ajouter une tâche">');
+	// });
+
+	$('button').on('click', function(){
+	$( ".container" ).append("<p>test</p>");
+		// $(this).after($('#input').text(););
+	// });
+
+});
+});
+
