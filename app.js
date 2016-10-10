198a9f5d1b6dfe688ff6f3d93a3144e1819c4fb7
marineco <marinecolonge@gmail.com>
2016-10-10 14:43:39 +0200

    Bouton " ajout list " check

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ $(document).ready(function() {
 	// $( ".container" ).delegate( "button", "click", function() {
 	// 	$(this).after('<input type="text" id="input" placeholder="Ajouter une tâche">');
 	// });
+	// var test = $('#input').val();
+	// var resultat = 	$('li').html(test)
 
 	$('button').on('click', function(){
-	$( ".container" ).append("<p>test</p>");
-		// $(this).after($('#input').text(););
-	// });
 
-});
+		var ajouter = $('input[name=list]').val();
+
+		$( "ul" ).append('<li>' + ajouter + '</li>');
+
+
+	});
 });
 
