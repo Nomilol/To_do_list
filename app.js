5835d65621bba8db67c72739c3f7fbd133b330a0
marineco <marinecolonge@gmail.com>
2016-10-12 11:28:54 +0200

    modif version avec seulement le bouton qui fonctionne 2e bouton en cours de test

--- app.js
+++ app.js
@@ -1,118 +1,65 @@
 
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
-		var compteur = -1;
-		var tab = [];
-		
-		
+
+	var compteur = 0;
 
 	$('.add').on('click', function(){
-		var obj = {
-			value: $('#input').val(),
-			status: false,
-
-		}
+		var ajouter = $('input[name=list]').val();		
 
 		compteur++
-		
-		tab.push(obj);
-		console.log(tab);
 
-		// $(this).attr('id', newId);
-
-		// });
-
-		$( "ul" ).append('<li class="aDetacher" > <input type="checkbox" data-number="'+compteur+'" id="checking'+compteur+'" class="check"> <label name="label" class="barrer">' + obj.value +'</label></li>');
-		
-
-		$('.check').on('click', function(){
-
-			var data = $(this).data('number');
-			
-			if (checked() === true) {
-
-				console.log(data)
-
-				tab[data].status = true;
-
-			}
-		});
+		$( "ul" ).append('<li class="aDetacher">' + '<input id="toDo'+compteur+'" type="checkbox" class="check">' + '<label name="label" class="barrer">' + ajouter +'</label>' + '</li>');		
 
 	});
-
-	function checked(){
-	 return $('.check').is(':checked')
-	}
 
 
 	// $('.buttonToDo').on('click', function(){
 
-
-	// 	if (verif1 === true){
-	// 		$('input[type=checkbox]').toggle('fast');
-	// 		$($('input[type=checkbox]').next('label')).toggle('fast');
-	// 	}else{
+	// 	function checked(){
+	// 		var check = $('input[type=checkbox]').is(':checked') 
+	// 		return check;
 
 	// 	}
+
+	// 	for(var i = 0; i <= compteur; i++){
+
+
+	// 		if (!checked() === false) {
+
+	// 		$('#toDo'+i).removeClass("check").addClass('unchecked');
+
+	// 			$('.unchecked').toggle('fast');
+	// 			$($('.unchecked').next('label')).toggle('fast')
+
+
+	// 			if (!checked() === false){
+
+	// 				$('#input').removeClass("check").addClass('checked');
+	// 			}
+
+	// 		} 
+	// 	}
+
 	// });
 
 
-	// $('.buttonDone').on('click', function(){
+	$('.buttonDone').on('click', function(){
 
-	// 	var verif2 = $('input[type=checkbox]').is(':checked');
-	// 	console.log(verif2);
 		
-	// 	if (verif2 === true) { 
+		var verif2 = $('input[type=checkbox]').is(':checked');
+		console.log(verif2);
 
-	// 		$('input[type=checkbox]:checked').toggle("slow");
-	// 		$($('input[type=checkbox]:checked').next("label")).toggle("slow");
-	// 	}
+		if (verif2 === true) { 
 
+			$('input[type=checkbox]:checked').toggle("slow");
+			$($('input[type=checkbox]:checked').next("label")).toggle("slow");
+		}
 
-
-
-	// });
-
-
+		checked();
 
 
+	});
 
-
-
-
-
-	// var btn1; 
-	// var btn2;
-	
-	
-	// $('.btn1').on('click', function(){
-	// 	if (btn1) {
-	// 		btn1.appendTo("ul");
-	// 		btn1 = null;
-
-	// 	} else {
-
-	// 		btn1 = $('.aDetacher').detach();
-	// 	}
-	// });
-
-
-	// $('.btn2').on('click', function(){
-
-	// 	if (btn2)	 {
-	// 		btn2.appendTo("ul");
-	// 		btn2 = null;
-
-	// 	} else {
-
-	// 		btn2 = $('.aDetacher').detach();
-	// 	}
-	// });
-
-	// function verif(){
-	// 	return $('input[type=checkbox]').is(':checked'); 
-	// 	console.log(!verif());
-	// }
-// });
-
+});
